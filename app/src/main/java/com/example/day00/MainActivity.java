4a0467ec47971fa4dab8181e197474851323d78d
xiaoxin-aosi <3042874090@qq.com>
2020-12-15 16:03:50 +0800

    这是一个随机测试

---
+++ app/src/main/java/com/example/day00/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.day00;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
